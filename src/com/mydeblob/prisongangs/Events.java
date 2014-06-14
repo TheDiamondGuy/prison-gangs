cf1a01008d1af13b679ddba4e4c5e9ac0d3cc35f
mydeblob <aaron.moss@mac.com>
2014-06-14 00:08:32 -0600

    Fixed bugs in event handler Started writing messages.yml Started organizing lang class

--- src/com/mydeblob/prisongangs/Events.java
+++ src/com/mydeblob/prisongangs/Events.java
@@ -12,62 +12,64 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Events implements Listener{
-    public static final GangManager t = GangManager.getInstance();
+	private static GangManager gm = GangManager.getGangManager();
+	private static FileManager f = FileManager.getFileManager();
 	private PrisonGangs plugin;
 	public Events(PrisonGangs plugin){
 		this.plugin = plugin;
 	}
 	@EventHandler(priority = EventPriority.HIGH)
-	public void onChat(AsyncPlayerChatEvent event){
-		if(t.getPlayerClan(event.getPlayer()) != null){
-			String pClan = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("format")).replaceAll("%g%", GangManager.getInstance().getPlayerClan(event.getPlayer()).getName());
-			String f = event.getFormat().replaceAll("\\{clan_name\\}", pClan);
-			event.setFormat(f);
+	public void onChat(AsyncPlayerChatEvent e){
+		Player p = e.getPlayer();
+		if(gm.getGangWithPlayer(p) != null){
+			Gang g = gm.getGangWithPlayer(p);
+			String pClan = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("format")).replaceAll("%g%", g.getName());
+			String f = e.getFormat().replaceAll("\\{gang\\}", pClan).replaceAll("\\{GANG\\}", pClan);
+			e.setFormat(f);
 		}else{
-				String f = event.getFormat().replaceAll("\\{clan_name\\}", null);
-				event.setFormat(f);
+			String f = e.getFormat().replaceAll("\\{gang\\}", null).replaceAll("\\{GANG\\}", null);
+			e.setFormat(f);
 		}
 	}
+
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event){
 		Player p = (Player) event.getPlayer();
-		if(!p.hasPlayedBefore() || !plugin.getGangConfig().contains("players." + p.getUniqueId().toString())){
-			plugin.getGangConfig().set("players." + p.getUniqueId().toString() + ".kills", 0);
-			plugin.getGangConfig().set("players." + p.getUniqueId().toString() + ".deaths", 0);
-			plugin.getGangConfig().set("players." + p.getUniqueId().toString() + ".kdr", 0);
+		if(!p.hasPlayedBefore() || !f.getGangConfig().contains("players." + p.getUniqueId().toString())){
+			f.getGangConfig().set("players." + p.getUniqueId().toString() + ".kills", 0);
+			f.getGangConfig().set("players." + p.getUniqueId().toString() + ".deaths", 0);
+			f.getGangConfig().set("players." + p.getUniqueId().toString() + ".kdr", 0);
 		}
 	} 
 	@EventHandler
-	public void onDeath(PlayerDeathEvent event){
-		int deaths = plugin.getGangConfig().getInt("players." + event.getEntity().getName() + ".deaths");
-		deaths++;
-		plugin.getGangConfig().set("players." + event.getEntity().getName() + ".deaths", deaths);
-		double edeaths = (double) plugin.getGangConfig().getInt("players." + event.getEntity().getName() + ".deaths");
-		double ekills = (double) plugin.getGangConfig().getInt("players." + event.getEntity().getName() + ".kills");
-		if(plugin.getGangConfig().getInt("players." + event.getEntity().getName() + ".kills") == 0 || plugin.getGangConfig().getDouble("players." + event.getEntity().getName() + ".kdr") < 0.000){
-			plugin.getGangConfig().set("players." + event.getEntity().getName() + ".kdr", 0.0);
+	public void onDeath(PlayerDeathEvent e){
+		Player victim = e.getEntity();
+		int victimDeaths = f.getGangConfig().getInt("players." + victim.getUniqueId().toString() + ".deaths");
+		victimDeaths++;
+		f.getGangConfig().set("players." + victim.getUniqueId().toString() + ".deaths", victimDeaths);
+		double dVictimDeaths = (double) f.getGangConfig().getInt("players." + victim.getUniqueId().toString() + ".deaths");
+		double victimKills = (double) f.getGangConfig().getInt("players." + victim.getUniqueId().toString() + ".kills");
+		if(f.getGangConfig().getInt("players." + victim.getUniqueId().toString() + ".kills") == 0 || f.getGangConfig().getDouble("players." + victim.getUniqueId().toString() + ".kdr") < 0.000){
+			f.getGangConfig().set("players." + victim.getUniqueId().toString() + ".kdr", 0.0);
 		}else{
-			plugin.getGangConfig().set("players." + event.getEntity().getName() + ".kdr", ekills/edeaths);
+			f.getGangConfig().set("players." + victim.getUniqueId().toString() + ".kdr", victimKills/dVictimDeaths);
 		}
-		if(event.getEntity().getKiller() instanceof Player){
-			Player killer = (Player) event.getEntity().getKiller();
-			int kills = plugin.getGangConfig().getInt("players." + killer.getName() + ".kills");
-			kills++;
-			plugin.getGangConfig().set("players." + killer.getName() + ".kills", kills);
-			double ddeaths = (double) plugin.getGangConfig().getInt("players." + event.getEntity().getKiller().getName() + ".deaths");
-			double dkills = (double) plugin.getGangConfig().getInt("players." + event.getEntity().getKiller().getName() + ".kills");
-			if(plugin.getGangConfig().getInt("players." + killer.getName() + ".deaths") == 0){
-				double k = (double) kills;
-				plugin.getGangConfig().set("players." + killer.getName() + ".kdr", k);
+		if(victim.getKiller() instanceof Player){
+			Player killer = (Player) e.getEntity().getKiller();
+			int killerKills = f.getGangConfig().getInt("players." + killer.getUniqueId().toString() + ".kills");
+			killerKills++;
+			f.getGangConfig().set("players." + killer.getUniqueId().toString() + ".kills", killerKills);
+			double killerDeaths = (double) f.getGangConfig().getInt("players." + killer.getUniqueId().toString() + ".deaths");
+			double dKillerKills = (double) f.getGangConfig().getInt("players." + killer.getUniqueId().toString() + ".kills");
+			if(f.getGangConfig().getInt("players." + killer.getUniqueId().toString() + ".deaths") == 0){
+				double k = (double) killerKills;
+				f.getGangConfig().set("players." + killer.getUniqueId().toString() + ".kdr", k);
 			}else{
-				plugin.getGangConfig().set("players." + killer.getName() + ".kdr", dkills/ddeaths);
+				f.getGangConfig().set("players." + killer.getUniqueId().toString() + ".kdr", dKillerKills/killerDeaths);
 			}
 		}
 	}
-	@EventHandler
-	public void playerJoin(PlayerJoinEvent event){
-		GangManager.getInstance().setupClans();
-	}
+
 	@EventHandler
 	public void onDamage(EntityDamageByEntityEvent event){
 		if(event.getEntity() instanceof Player){
@@ -75,17 +77,20 @@ public class Events implements Listener{
 			Player damager = null;
 			if(event.getDamager() instanceof Player){
 				damager = (Player) event.getDamager();
-			}if(event.getDamager() instanceof Projectile){
+			}else if(event.getDamager() instanceof Projectile){
 				Projectile proj = (Projectile) event.getDamager();
 				if(proj.getShooter() instanceof Player){
 					damager = (Player) proj.getShooter();
 				}
 			}
-			if(GangManager.getInstance().getPlayerClan(target) == null){
+			if(damager == null){
 				return;
-			}if(GangManager.getInstance().getPlayerClan(damager) == null){
+			}
+			if(gm.getGangWithPlayer(target) == null){
 				return;
-			}if(GangManager.getInstance().getPlayerClan(target).equals(GangManager.getInstance().getPlayerClan(damager))){
+			}if(gm.getGangWithPlayer(damager) == null){
+				return;
+			}if(gm.getGangWithPlayer(target) == gm.getGangWithPlayer(damager)){
 				event.setCancelled(true);
 			}
 		}
