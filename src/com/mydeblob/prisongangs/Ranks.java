dfc1e82203516fb3e7cd5c92c80075798fbb78c5
CullanP <cullanpage@gmail.com>
2014-11-22 13:04:32 -0800

    Update Ranks.java

--- src/com/mydeblob/prisongangs/Ranks.java
+++ src/com/mydeblob/prisongangs/Ranks.java
@@ -1,26 +1,26 @@
-package com.mydeblob.prisongangs;
+package com.cullan.prisongangs;
 
-
-public enum Ranks {
-	MEMBER,
-	TRUSTED,
-	OFFICER,
-	LEADER,
-	OWNER;
-	
-	public String toText(){
-		if(this == MEMBER){
-			return "Member";
-		}else if(this == TRUSTED){
-			return "Trusted";
-		}else if(this == OFFICER){
-			return "Officer";
-		}else if(this == LEADER){
-			return "Leader";
-		}else if(this == OWNER){
-			return "Owner";
-		}else{
-			return null;
-		}
-	}
+public enum Ranks
+{
+  MEMBER,  TRUSTED,  OFFICER,  LEADER,  OWNER;
+  
+  public String toText()
+  {
+    if (this == MEMBER) {
+      return "Member";
+    }
+    if (this == TRUSTED) {
+      return "Trusted";
+    }
+    if (this == OFFICER) {
+      return "Officer";
+    }
+    if (this == LEADER) {
+      return "Leader";
+    }
+    if (this == OWNER) {
+      return "Owner";
+    }
+    return null;
+  }
 }
